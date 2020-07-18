0cbfaaeb7585768b0332aafe8ef2578769e1a3a7
rulyox <email@rulyox.com>
2020-07-18 20:45:49 +0900

    Added data binding library

--- app/src/main/java/com/rulyox/mvvm/MainActivity.kt
+++ app/src/main/java/com/rulyox/mvvm/MainActivity.kt
@@ -2,14 +2,16 @@ package com.rulyox.mvvm
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
+import com.rulyox.mvvm.databinding.ActivityMainBinding
 
 class MainActivity: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        setSupportActionBar(main_toolbar)
+
+        val binding: ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.mainToolBar)
 
     }
 
