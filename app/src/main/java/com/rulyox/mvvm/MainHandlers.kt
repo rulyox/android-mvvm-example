f21aa0666c897d1666a2706b4b73f0a52cea3129
rulyox <email@rulyox.com>
2020-07-18 21:29:27 +0900

    Added main handler

---
+++ app/src/main/java/com/rulyox/mvvm/MainHandlers.kt
@@ -0,0 +1,16 @@
+package com.rulyox.mvvm
+
+import android.content.Intent
+import android.view.View
+import com.rulyox.mvvm.activity.AddActivity
+
+class MainHandlers {
+
+    fun clickAddButton(view: View) {
+
+        val addIntent = Intent(view.context, AddActivity::class.java)
+        view.context.startActivity(addIntent)
+
+    }
+
+}
--- app/src/main/java/com/rulyox/mvvm/activity/MainActivity.kt
+++ app/src/main/java/com/rulyox/mvvm/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.rulyox.mvvm
+package com.rulyox.mvvm.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.rulyox.mvvm.MainHandlers
 import com.rulyox.mvvm.databinding.ActivityMainBinding
 
 class MainActivity: AppCompatActivity() {
@@ -12,6 +13,7 @@ class MainActivity: AppCompatActivity() {
         val binding: ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setSupportActionBar(binding.mainToolBar)
+        binding.handlers = MainHandlers()
 
     }
 
